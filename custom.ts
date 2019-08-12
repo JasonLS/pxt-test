19c341d336d51f291aa5deabf7d8005b8c17459d
JasonLS <jsedluk01@gmail.com>
2019-08-12 09:57:41 -0400

    Updates.

--- custom.ts
+++ custom.ts
@@ -1,19 +1,12 @@
 namespace robot {
 
     export class MyRobot extends Robot {
+        constructor(){super()}
 
-        constructor() {
-            super()
-            
-            
-        }
-        
-        move():void{
+        move(){
             music.baDing.play()
         }
     }
-
     robot.currentRobot = new MyRobot(); // libraries can set this field when loading
-
 }
 
