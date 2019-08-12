30d62606eb27e2342fcc503ff548708ecd95a0d5
JasonLS <jsedluk01@gmail.com>
2019-08-12 10:27:48 -0400

    Updates.

--- custom.ts
+++ custom.ts
@@ -1,13 +1,1 @@
-//% weight=10 color=#006400 icon="\uf1b9" block="Tinybit"
-namespace robot {
-
-    export class MyRobot extends Robot {
-        constructor(){super()}
-
-        move(){
-            music.baDing.play()
-        }
-    }
-    robot.currentRobot = new MyRobot(); // libraries can set this field when loading
-}
-
+ 
